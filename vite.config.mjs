d399298ba29431cd7a915f51a22749084eab3b4d
Andrei Belousov <110045588+ponyn1ce@users.noreply.github.com>
2025-11-20 03:24:34 +0300

    Delete vite.config.mjs

--- vite.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Vite config in ESM to avoid CJS deprecation warning
-// https://vite.dev/guide/troubleshooting.html#vite-cjs-node-api-deprecated
-export default defineConfig({
-  plugins: [react()],
-  base: './', // <- ОБЯЗАТЕЛЬ НО перед сборкой
-})
